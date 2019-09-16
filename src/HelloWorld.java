ccafe6ec03bee4cb12ef7de20e2b760e486fa46e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-16 16:49:12 -0500

    complete exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,87 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Universe");
+//        int myFavoriteNumber = 13;
+//        System.out.println(myFavoriteNumber);
+
+//        String myString = "Ryan";
+//        System.out.println(myString);
+
+//        long myNumber;
+//
+//        myNumber = 123L;
+//®
+//        System.out.println(myNumber);
+//
+//        myNumber = 123;
+
+//        double myNumber = 3.14;
+//
+//        double newNumber = (float) myNumber;
+//
+//        System.out.println(newNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        System.out.println(o);
+
+//        int x = 4;
+//        x = x + 5;
+
+//        x += 5;
+
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        y *= x;
+
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+
+//        x = x / y;
+//        y = y - x;
+
+//        x /= y;
+//
+//        y -= x;
+//
+//        System.out.println(x);
+//
+//        System.out.println(y);
+
+        short x = 200;
+        int y = x;
+
+        System.out.println(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }

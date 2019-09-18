b58347de6f3f1af5766ef334bfad1bf213d2d707
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-18 12:17:25 -0500

    add solutions

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,6 +1,36 @@
 public class StringExercise {
     public static void main(String[] args) {
 
+//        For each of the following output examples: create a String variable named message that contains the desired output and print it out to the console.
+//
+//        Do this with only one String variable and one print statement.
+
+        //STRING ONE
+
+        String messageOne = "We don't need no education \nWe don't need no thought control";
+
+        System.out.println(messageOne);
+
+        //  STRING TWO
+
+        String messageTwo = "Check \"this\" out!, \"s inside of \"s!";
+
+        System.out.println(messageTwo);
+
+        // STRING THREE
+
+        String messageThree = "In windows, the main drive is usually C:\\";
+
+        System.out.println(messageThree);
+
+        // STRING FOUR
+
+        String messageFour = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(messageFour);
+
+
 
 
 

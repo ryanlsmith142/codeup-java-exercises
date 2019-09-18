4ed0f522df0e0e2b8a549952e0823ec646a588b9
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-18 13:55:20 -0500

    finish exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
-    }
-}
+        //ALLOWS USER TO TYPE SOMETHING TO BOB
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Say something to Bob and hit ENTER");
+
+        String bobSaying = userInput.nextLine();
+
+        //CHECKS TO SEE WHAT BOB WILL SAY BACK
+
+        String message;
+
+        if(bobSaying.endsWith("?")) {
+            message = "Sure";
+        } else if(bobSaying.endsWith("!")) {
+            message = "Whoa chill out";
+        } else if(bobSaying.equals("")) {
+            message = "Fine, be that way";
+        } else {
+            message = "whatever";
+        }
+
+        System.out.println(message);
+
+
+
+
+    } //main()
+} //Bob Class

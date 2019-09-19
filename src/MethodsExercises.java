a135c40f4fb1b6fcc0f5d64b82127584eab741d2
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-19 10:46:21 -0500

    add addition solution

---
+++ src/MethodsExercises.java
@@ -0,0 +1,35 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+        System.out.println(addition(1,2));
+
+
+//        Basic Arithmetic
+//
+//        Create four separate methods. Each will perform an arithmetic operation:
+//
+//        Addition
+
+//                Subtraction
+
+//        Multiplication
+
+//                Division
+
+//        Each function needs to take two parameters/arguments so that the operation can be performed.
+//
+//                Test your methods and verify the output.
+//
+//        Add a modulus method that finds the modulus of two numbers.
+//
+//                Food for thought: What happens if we try to divide by zero? What should happen?
+    }
+
+    public static int addition(int num1, int num2) {
+
+        return num1 + num2;
+
+    }
+
+    public static int subtraction
+}

cd6bb2cd8553913599da062906dd1af1587d2ef1
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-26 15:03:54 -0500

    add Animal class, private variables and add getters and setters

---
+++ src/animal/Animal.java
@@ -0,0 +1,31 @@
+package animal;
+
+public class Animal {
+    private String age;
+    private String species;
+    private boolean isAlive;
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSpecies() {
+        return this.species;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public boolean getIsAlive() {
+        return this.isAlive;
+    }
+}
